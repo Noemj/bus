17d52a285ae78ad0db0c1c23a4f5bdbdb21168b9
Noemj <olli.rissanen@helsinki.fi>
2013-01-23 23:20:22 +0200

    Support for multiple routes

--- bus.py
+++ bus.py
@@ -11,17 +11,19 @@ else:
 
 xml = urllib2.urlopen('http://api.reittiopas.fi/public-ytv/fi/api/?a=2556309,6679691&b=2553603,6677524&time=' + time + '&timemode=1&user=username&pass=password')
 timetable = parse(xml)
-routes = timetable.getElementsByTagName('ROUTE')[0]
-lines = routes.getElementsByTagName('LINE')[0]
-stops = lines.getElementsByTagName('STOP')
+routes = timetable.getElementsByTagName('ROUTE')
+for route in routes:
+    lines = route.getElementsByTagName('LINE')
+    for line in lines:   
+        stops = line.getElementsByTagName('STOP')
 
-for stop in stops:
-    name = stop.getElementsByTagName('NAME')
-    val = map(lambda p: p.getAttribute('val'),name)
-    if 'A.I. Virtasen aukio' in val:
-        arrival = stop.getElementsByTagName('ARRIVAL')
-        time = map(lambda p: p.getAttribute('time'),arrival)
-        print 'Next 506 leaving from Kumpula at: ' + str(time).split('\'')[1]
-        sys.exit(0)
+        for stop in stops:
+            name = stop.getElementsByTagName('NAME')
+            val = map(lambda p: p.getAttribute('val'),name)
+            if 'A.I. Virtasen aukio' in val:
+                arrival = stop.getElementsByTagName('ARRIVAL')
+                time = map(lambda p: p.getAttribute('time'),arrival)
+                print 'Next 506 leaving from Kumpula at: ' + str(time).split('\'')[1]
+                sys.exit(0)
 
 print 'Vika meni jo.. '
